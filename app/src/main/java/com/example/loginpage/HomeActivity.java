ce67ffcb738acdc9dcc9d713036cb5ab8680e96e
Alfira141099 <57779369+Alfira141099@users.noreply.github.com>
2020-05-17 23:04:30 +0700

    E31180332
    Dewi Ratih T. W

--- app/src/main/java/com/example/loginpage/HomeActivity.java
+++ app/src/main/java/com/example/loginpage/HomeActivity.java
@@ -2,7 +2,11 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.loginpage.ui.InputPemasokan;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+    }
+    public void Pemasokan(View view) {
+        Intent intent = new Intent(HomeActivity.this, InputPemasokan.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/loginpage/ui/InputPemasokan.java
@@ -0,0 +1,14 @@
+package com.example.loginpage.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputPemasokan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_pemasokan);
+    }
+}
